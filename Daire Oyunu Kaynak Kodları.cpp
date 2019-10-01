bbe4592222abb95b285880b5fc3a8b6e622c51d9
Ahmet Bohur <ahmetbohur@icloud.com>
2019-10-01 20:42:32 +0300

    Daire Oyunu KK v1.02

--- Daire Oyunu Kaynak Kodları.cpp
+++ Daire Oyunu Kaynak Kodları.cpp
@@ -1,13 +1,10 @@
-﻿// ConsoleApplication1.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <conio.h>
 
 
 int main()
-{	
-    int a, b, d, k;	
+{
+	int a, b, d, k;
 	int c[1000];
 	setlocale(LC_ALL, "Turkish"); // Türkçe şeyler
 	printf("Matematik Daire Oyun \n Oyunda N kadar karakter daire oluşturuluyor, bu ortamda 1 adet kılıç var ve 1. kişiden oyun başlıyor. \n Birinci kişi sağındaki kişiye kılıcı saplıyor ve öldürüyor ardından yanındaki ölmeyen kişiye kılıcı veriyor ve o da aynısını sağındaki kişi için yapıyor ve oyun sonda 1 kişi kalana kadar devam ediyor. \n N sayısına göre hangi sıradaki kişinin hayatta kaldığını bulalım.");
@@ -16,10 +13,11 @@ int main()
 
 	for (int i = 0; i < a; i++) {
 		b = pow(2, i);
-		if (a > b) {			
+		if (a > b) {
 			c[i] = b;
-			
-		}else{		
+
+		}
+		else {
 			if (i > 1) {
 				k = i - 1;
 			}
@@ -27,15 +25,13 @@ int main()
 				k = 1;
 			}
 			d = 1 + 2 * (a - c[k]);
-			std::cout << a << " Kişi arasından kurtulacak şanslı kişi : " << d <<".Sıradaki kişi. \n\n";
-	
+			std::cout << a << " Kişi arasından kurtulacak şanslı kişi : " << d << ".Sıradaki kişi. \n\n";
+
 			break;
-			
+
 		}
-	}	
+	}
 	system("PAUSE");
 	return 0;
 
 }
-
-
