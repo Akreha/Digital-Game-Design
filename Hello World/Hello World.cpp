38576e0f289ef663db369190bc937e2abb89f428
Ahmet Bohur <ahmetbohur@icloud.com>
2019-10-08 11:27:28 +0300

    Hello World

---
+++ Hello World/Hello World.cpp
@@ -0,0 +1,15 @@
+
+// Kütüphaneler
+#include "pch.h"
+#include <iostream>
+
+using namespace std; // std:: yi sürekli koymak zorunda kalmamak için yazdýðýmýz kod.
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	printf("Hello World \n"); // Printf ile yazý
+	cout << "Merhaba Osman Bey hayat nasýl gidiyor? \n"; // Cout ile yazý 
+
+}
+
