b96fd7ce7c61d78924e7d666b103b6958df8d8a6
Ahmet Bohur <ahmetbohur@icloud.com>
2019-10-09 00:39:14 +0300

    Sayı Tahmin Tablosu Oluşturma
    Daha hızlı ve daha kısa bir kod yapısı oluşuturuldu.

--- Sayı Tahmin Tablosu Oluşturma/Sayı Tahmin Tablosu Oluşturma.cpp
+++ Sayı Tahmin Tablosu Oluşturma/Sayı Tahmin Tablosu Oluşturma.cpp
@@ -1,213 +1,122 @@
 ﻿#include <iostream>
-#include <conio.h>
-#include <math.h>
-#include <windows.h>
+#include <Windows.h>
 
 using namespace std;
-int main() {
-	setlocale(LC_ALL, "Turkish");
 
-	int bulunacak_Sayi;
-basaal:
-	cout << "Lütfen tablosu oluşturulcak sayıyı girin(1 - 256 Arasında olması gerekiyor, 256 dahil değil!) : ";
-	cin >> bulunacak_Sayi;
-	//int* kareler = new int[bulunacak_Sayi];
-	if (bulunacak_Sayi >= 256) {
-		goto basaal;
-	}
-	else {
-		int sayi = 1, sayac = 0, kare_sayisi = 0;
-		for (int i = 0; i < bulunacak_Sayi; i++) {
-			int kare = pow(2, i);
-			if (kare <= bulunacak_Sayi) {
-				kare_sayisi++;
-			}
-			else {
-				for (int c = 1; c <= kare_sayisi; c++) {
+int main()
+{
+	basadon:
+	setlocale(LC_ALL, "turkish");
+	int girilen_Sayi, kare, sayac=0, kare_max=0, sayac_2, sayilar_kontrol, sayi;
+	
+	cout << "Lütfen tablosunu oluşturmak istediğiniz sayıyı girin : ";
+	cin >> girilen_Sayi;
+	
+	int *kareler = new int[girilen_Sayi];
 
-					cout << "Tablo " << c << "\n";
-					sayac = 0;
-					int sayi_tespit = 1;
-					while (sayac <= bulunacak_Sayi) {
-						// 1. Tablo
-
-						if (c == 1 && sayi % 2 != 0) {
-							if (sayi < 256) { if (sayi == 1) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2); } else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7); } cout << sayi << "\n"; }
-							sayi++;
-							sayac++;
-						}
-						else if (c == 1 && sayi % 2 == 0) {
-							sayi++;
-							sayac++;
-						}
-
-						// 2. Tablo
-						if (c == 2 && sayac == 0) {
-							sayac++;
-							sayi = 2;
+	for (int i = 0; i < girilen_Sayi; i++) {
+		kare = pow(2, i);
+		if (kare <= girilen_Sayi) {
+			kareler[i] = kare;
+			kare_max++;
+			// cout << kareler[i];
+		}
+		else {
+			while (sayac < kare_max) {
+				cout << "\n" << sayac + 1 << ".Tablo :";
+				sayilar_kontrol = 0;
+				sayi = 0;
+				sayac_2 = 0;
+				while (sayac_2 <= girilen_Sayi) {
+					sayac_2++;
+					
+					
+					if (sayac == 0 && sayac_2 % 2 != 0) {
+						if (sayac_2 == girilen_Sayi) {
 							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2);
-							if (sayi < 256) { cout << sayi << "\n"; }
 						}
-						else if (c == 2 && sayi % 2 == 0) {
+						else {
 							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
-							sayi++;
-							sayac++;
-							if (sayi < 256) { if (sayi == bulunacak_Sayi) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2); } else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7); } cout << sayi << "\n"; }
 						}
-						else if (c == 2 && sayi % 2 != 0) {
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
-							sayi = sayi + 3;
-							sayac++;
-							if (sayi < 256) { if (sayi == bulunacak_Sayi) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2); } else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7); } cout << sayi << "\n"; }
+						cout << sayac_2 << " ";
+					}
+					
+					if (sayac == 1 && sayilar_kontrol % 2 == 0) {				
+						if (sayilar_kontrol == 0) {
+							sayilar_kontrol = 1;
 						}
-
-						// 3. Tablo
-						if (c == 3 && sayac == 0) {
-							sayac++;
-							sayi = 4;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2);
-							if (sayi < 256) { cout << sayi << "\n"; }
+						sayilar_kontrol++;
+						if (sayilar_kontrol <= girilen_Sayi) {
+							if (sayilar_kontrol == girilen_Sayi) {
+								SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2);
+							}
+							else {
+								SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
+							}
+							cout << sayilar_kontrol << " ";
 						}
-						else if (c == 3 && sayi_tespit < 4) {
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
-							sayi_tespit++;
-							sayac++;
-							sayi = sayi + 1;
-							if (sayi < 256) { if (sayi == bulunacak_Sayi) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2); } else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7); } cout << sayi << "\n"; }
+					}
+					else if (sayac == 1 && sayilar_kontrol % 2 != 0) {
+						sayilar_kontrol = sayilar_kontrol + 3;
+						if (sayilar_kontrol <= girilen_Sayi) {
+							if (sayilar_kontrol == girilen_Sayi) {
+								SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2);
+							}
+							else {
+								SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
+							}
+							cout << sayilar_kontrol << " ";
 						}
-						else if (c == 3 && sayi_tespit == 4) {
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
-							sayac++;
-							sayi_tespit = 1;
-							sayi = sayi + 5;
-							if (sayi < 256) { if (sayi == bulunacak_Sayi) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2); } else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7); } cout << sayi << "\n"; }
-						}
-						// 4. Tablo
-
-						if (c == 4 && sayac == 0) {
-							sayac++;
-							sayi = 8;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2);
-							if (sayi < 256) { cout << sayi << "\n"; }
-						}
-						else if (c == 4 && sayi_tespit < 8) {
-							sayi_tespit++;
-							sayac++;
-							sayi = sayi + 1;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
-							if (sayi < 256) { if (sayi == bulunacak_Sayi) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2); } else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7); } cout << sayi << "\n"; }
-						}
-						else if (c == 4 && sayi_tespit == 8) {
-							sayac++;
-							sayi_tespit = 1;
-							sayi = sayi + 7;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
-							if (sayi < 256) { if (sayi == bulunacak_Sayi) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2); } else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7); } cout << sayi << "\n"; }
-						}
-
-						// 5. Tablo
-
-						if (c == 5 && sayac == 0) {
-							sayac++;
-							sayi = 16;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2);
-							if (sayi < 256) { cout << sayi << "\n"; }
-						}
-						else if (c == 5 && sayi_tespit < 16) {
-							sayi_tespit++;
-							sayac++;
-							sayi = sayi + 1;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
-							if (sayi < 256) { if (sayi == bulunacak_Sayi) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2); } else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7); } cout << sayi << "\n"; }
-						}
-						else if (c == 5 && sayi_tespit == 16) {
-							sayac++;
-							sayi_tespit = 1;
-							sayi = sayi + 7;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
-							if (sayi < 256) { if (sayi == bulunacak_Sayi) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2); } else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7); } cout << sayi << "\n"; }
-						}
-
-						// 6. Tablo
-
-						if (c == 6 && sayac == 0) {
-							sayac++;
-							sayi = 32;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2);
-							if (sayi < 256) { cout << sayi << "\n"; }
-						}
-						else if (c == 6 && sayi_tespit < 32) {
-							sayi_tespit++;
-							sayac++;
-							sayi = sayi + 1;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
-							if (sayi < 256) { if (sayi == bulunacak_Sayi) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2); } else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7); } cout << sayi << "\n"; }
-						}
-						else if (c == 6 && sayi_tespit == 32) {
-							sayac++;
-							sayi_tespit = 1;
-							sayi = sayi + 9;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
-							if (sayi < 256) { if (sayi == bulunacak_Sayi) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2); } else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7); } cout << sayi << "\n"; }
-						}
-
-
-						// 7. Tablo
-
-						if (c == 7 && sayac == 0) {
-							sayac++;
-							sayi = 64;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2);
-							if (sayi < 256) { cout << sayi << "\n"; }
-						}
-						else if (c == 7 && sayi_tespit < 64) {
-							sayi_tespit++;
-							sayac++;
-							sayi = sayi + 1;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
-							if (sayi < 256) { if (sayi == bulunacak_Sayi) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2); } else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7); } cout << sayi << "\n"; }
-						}
-						else if (c == 7 && sayi_tespit == 64) {
-							sayac++;
-							sayi_tespit = 1;
-							sayi = sayi + 11;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
-							if (sayi < 256) { if (sayi == bulunacak_Sayi) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2); } else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7); } cout << sayi << "\n"; }
-						}
-
-
-						// 8. Tablo
-
-						if (c == 8 && sayac == 0) {
-							sayac++;
-							sayi = 128;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2);
-							if (sayi < 256) { cout << sayi << "\n"; }
-						}
-						else if (c == 8 && sayi_tespit < 128) {
-							sayi_tespit++;
-							sayac++;
-							sayi = sayi + 1;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
-							if (sayi < 256) { if (sayi == bulunacak_Sayi) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2); } else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7); } cout << sayi << "\n"; }
-						}
-						else if (c == 8 && sayi_tespit == 128) {
-							sayac++;
-							sayi_tespit = 1;
-							sayi = sayi + 13;
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
-							if (sayi < 256) { if (sayi == bulunacak_Sayi) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2); } else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7); } cout << sayi << "\n"; }
-						}
-
-
 					}
 
 
-				}
+					if (sayac >= 2) {
 
-				goto basaal;
-				break;
+						if (sayilar_kontrol == 0) {
+							sayilar_kontrol = kareler[sayac] - 1;
+						}
+						if (sayi == kareler[sayac]) {
+							sayi = 1;
+							sayilar_kontrol = sayilar_kontrol + kareler[sayac] + 1;
+							if (sayilar_kontrol <= girilen_Sayi) {
+								if (sayilar_kontrol == girilen_Sayi) {
+									SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2);
+								}
+								else {
+									SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
+								}
+								cout << sayilar_kontrol << " ";
+							}
+						}
+						else {
+							sayilar_kontrol++;
+							sayi++;
+							if(sayilar_kontrol <= girilen_Sayi){
+								if (sayilar_kontrol == girilen_Sayi) {
+									SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 2);
+								}
+								else {
+									SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
+								}
+								cout << sayilar_kontrol << " ";
+							}
+							
+						}
+					}
+
+					
+
+				}
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0 * 16 + 7);
+				cout << "\n";
+				sayac++;
 			}
+			cout << "\n";
+			goto basadon;
+			break;
 		}
 	}
+
 }
+
+
